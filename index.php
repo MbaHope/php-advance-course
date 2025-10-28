14bda198da56a9ce5e6e5d05b53b2950877c8df7
Hope Mbah <mbahope62@gmail.com>
2025-10-28 15:35:22 +0100

    Added Shape properties and methods to file

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class Fruit
     // properties
     public  $name;
     public  $color;
-
+    public $shape;
 
     //    methods
     public function set_name($name)
@@ -27,27 +27,43 @@ class Fruit
     {
         return $this->color;
     }
+
+    public function set_shape($shape)
+    {
+        return $this->shape = $shape;
+    }
 }
 
 
 $apple = new Fruit();
 $apple->set_name("Apple");
 $apple->set_color("Green");
+$apple->set_shape("Circle");
 
 $banana = new Fruit();
 $banana->set_name("Banana");
 $banana->set_color("Yellow");
+$banana->set_shape("Sphere");
 
 // echo $apple->get_name();
+echo "For Apple"; echo "<hr/>";
+echo "<br />";
+
 echo $apple->name;
 echo "<br />";
 // echo $banana->get_name();
-echo $banana->name;
+echo $apple->color;
+echo "<br />";
+echo $apple->shape;
 echo "<br />";
 echo "<br />";
 
+echo "For Banana";echo "<hr/>";
+echo "<br />";
 // echo $apple->get_color();
-echo $apple->color;
+echo $banana->name;
 echo "<br />";
 // echo $banana->get_color();
 echo $banana->color;
+echo "<br />";
+echo $banana->shape;
