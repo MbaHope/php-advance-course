9a758110f595210b135d1262a94a210a73f91fb1
Hope Mbah <mbahope62@gmail.com>
2025-10-28 16:48:50 +0100

    Added the destructor method

--- index.php
+++ index.php
@@ -25,6 +25,10 @@ class Fruit
     //     return $this->color;
     // }
 
+    public function __destruct()
+    {
+        echo "The fruit is " . $this->name . "," . " the color is " . $this->color . " and the shape is " . $this->shape . "<br />" . "<br />";
+    }
 }
 
 $apple = new Fruit("Apple", "Green", "Circle");
@@ -32,24 +36,24 @@ $apple = new Fruit("Apple", "Green", "Circle");
 $banana = new Fruit("Banana", "Yellow", "Sphere");
 
 // echo $apple->get_name();
-echo "For Apple";
-echo "<hr/>";
+// echo "For Apple";
+// echo "<hr/>";
 
-echo $apple->name;
-echo "<br />";
+// echo $apple->name;
+// echo "<br />";
 // echo $banana->get_name();
-echo $apple->color;
-echo "<br />";
-echo $apple->shape;
-echo "<br />";
-echo "<br />";
+// echo $apple->color;
+// echo "<br />";
+// echo $apple->shape;
+// echo "<br />";
+// echo "<br />";
 
-echo "For Banana";
-echo "<hr/>";
+// echo "For Banana";
+// echo "<hr/>";
 // echo $apple->get_color();
-echo $banana->name;
-echo "<br />";
+// echo $banana->name;
+// echo "<br />";
 // echo $banana->get_color();
-echo $banana->color;
-echo "<br />";
-echo $banana->shape;
+// echo $banana->color;
+// echo "<br />";
+// echo $banana->shape;
